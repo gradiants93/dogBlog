41bc6eb99804d28529c763d2433642121d148ae2
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-04-08 10:48:53 -0700

    Pulling and displaying posts from db

--- client/src/pages/blog.js
+++ client/src/pages/blog.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Blog = () => {
-  return <h1>You can write your blog!</h1>;
+  const [posts, setPosts] = useState([]);
+
+  const loadPosts = () => {
+    fetch("/api/posts")
+      .then((response) => response.json())
+      .then((posts) => {
+        setPosts(posts);
+        console.log(posts);
+      });
+  };
+  useEffect(() => {
+    loadPosts();
+  }, []);
+  return (
+    <>
+      <div className="blog">
+        <h2>You can write your blog!</h2>
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                {" "}
+                {post.title} {post.textcontent}
+                <img src={post.photourl} alt={post.alttext} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Blog;
